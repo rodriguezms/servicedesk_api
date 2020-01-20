b64dc6fadc77a8716a0babe5b424b4744ff45e16
Rodriguez Matias <matiass.rodriguez@telefonica.com>
2020-01-20 00:46:32 -0300

    Linter

--- invGateProject/urls.py
+++ invGateProject/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path(r'admin/', admin.site.urls),    
+    path(r'admin/', admin.site.urls),
     path(r'api/v1/', include('api.urls')),
 ]
